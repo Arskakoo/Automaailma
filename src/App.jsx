61c3a45f7210ed1cc851c9182717212d833647fe
arskakoo <aaron.kairavuo@gmail.com>
2023-09-15 13:09:41 +0300

    V 0.0.1

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import React from 'react';
+
+import Logo from './assets/img/logo.png'
+import './App.css'
+import autot from './assets/json/cars.json'
+
+function App() {
+
+  return (
+    <>
+
+      <header>
+        <img src={Logo} class="logo react" alt="React logo" />
+
+        <div class='navLinks'>
+        <a href="">Yhteystiedot</a>
+        <a href="">Autot</a>
+        </div>
+
+
+      </header>
+
+      <main>
+
+
+      <Cars />
+
+      </main>
+
+      <footer>
+
+      </footer>
+
+    </>
+  )
+}
+
+
+function Cars() {
+  return (
+    <div class="car-list">
+      {autot.cars.map((car, index) => (
+        <div class="car" key={index}>
+          <img src={car.kuva}/>
+          <h2>{car.nimi}</h2>
+          <h6>Vuosimalli: {car.vuosimalli}</h6>
+          {/* <h6>{car.merkki}</h6> */}
+          {/* <h6>{car.korimalli}</h6> */}
+          <h6>Hinta:  {car.hinta} €</h6>
+          <h6>{car.vaihteisto}</h6>
+          <h6>{car.mittarilukema} km</h6>
+{/* '          <h6>{car.lisätiedot}</h6> */}
+        <br />
+        <button class='showCarStats'>Tutustu</button>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+
+
+export default App
